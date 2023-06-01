8466cc4a1207045cdeeed2a4d2fd430d972607e4
JanefrancessC <janefrancesschioma@gmail.com>
2023-06-01 12:20:45 +0100

    successful MongoDB connection

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+// MongoDb connection
+const connect = () => {
+    mongoose.connect(process.env.MONGO_DB)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => {throw error});
+}
 
 app.get('/', (req, res) => {
     res.json({ message: "This is LangJo App 🏴󠁧󠁢󠁥󠁮󠁧󠁿!" });
@@ -18,5 +24,6 @@ app.use((req, res) => {
   });
 
 app.listen(PORT, () => {
+    connect()
     console.log(`Listening on port ${PORT}`);
   });

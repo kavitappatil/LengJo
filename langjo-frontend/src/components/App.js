a015072c8f8db9f1acc99097858b6c08c93d28b1
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-16 14:13:59 +0100

    Fixed files

---
+++ langjo-frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "../../../../LangJoP/client/langjo/src/components/NavBar/NavBar";
+import Pages from "../../../../LangJoP/client/langjo/src/components/Pages";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <Pages />
+      </div>
+    </div>
+  );
+}
+
+export default App;

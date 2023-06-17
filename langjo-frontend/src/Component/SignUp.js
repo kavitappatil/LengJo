eec562f697ee3feaefadaf2d12af3819eed0f96a
kavitappatil <patil.poem@gmail.com>
2023-06-17 22:14:36 +0100

    Sign uo page updated

--- langjo-frontend/src/Component/SignUp.js
+++ langjo-frontend/src/Component/SignUp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SignUp.css";
+import langJo from "../Images/langJo.png";
 
 function SignUp() {
   return (
@@ -7,55 +8,64 @@ function SignUp() {
       <div className="container h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
-            <div className="card text-black">
+            <div className="card text-black card-color">
               <div className="card-body p-md-5">
                 <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-1 order-lg-1">
+                    <p className="text-start h2 sign_up-heading fw-bold p-2">
                       Sign up
                     </p>
-
+                    <span className="form-group d-flex flex-row p-2">
+                      Already have an account?{" "}
+                      <span className="login">&nbsp; Log in</span>
+                    </span>
+                    &nbsp;
                     <form>
-                      <div className="form-group">
-                        <label for="exampleInputPassword1">Full Name</label>
+                      <div className="form-group ">
+                        <div className="d-flex flex-row p-2">
+                          <label for="exampleInputPassword1 d-flex  ">
+                            Full Name
+                          </label>
+                        </div>
                         <input
                           type="name"
-                          className="form-control"
+                          className="form-control w-75"
                           id="exampleInputName"
                           placeholder="Name"
                         />
                       </div>
-                      <div className="form-group"> 
-                        <label for="exampleInputEmail1">Email address</label>
+                      &nbsp;
+                      <div className="form-group">
+                        <div className="d-flex flex-row p-2">
+                          <label for="exampleInputEmail1">Email address</label>
+                        </div>
                         <input
                           type="email"
-                          className="form-control"
+                          className="form-control w-75"
                           id="exampleInputEmail1"
                           aria-describedby="emailHelp"
                           placeholder="Enter email"
                         />
                       </div>
+                      &nbsp;
                       <div className="form-group">
-                        <label for="exampleInputPassword1">Password</label>
+                        <div className="d-flex flex-row p-2">
+                          <label for="exampleInputPassword1">Password</label>
+                        </div>
                         <input
                           type="password"
-                          className="form-control"
+                          className="form-control w-75"
                           id="exampleInputPassword1"
                           placeholder="Password"
                         />
                       </div>
-                      <div className="form-group form-check">
-                        <input
-                          type="checkbox"
-                          className="form-check-input"
-                          id="exampleCheck1"
-                        />
+                      <br />
+                      <div className="row d-flex justify-content-start p-3">
+                        <button type="submit" className="btn sign_up-btn w-75">
+                          Sign Up
+                        </button>
                       </div>
-                      <button type="submit" className="btn btn-primary">
-                        Submit
-                      </button>
                     </form>
-
                     {/* <form className="mx-1 mx-md-4">
 
                   <div className="d-flex flex-row align-items-center mb-4">
@@ -95,15 +105,23 @@ function SignUp() {
 
                 </form> */}
                   </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-2 order-lg-2">
                     <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      src={langJo}
                       className="img-fluid"
                       alt="Sample image"
                     />
                   </div>
                 </div>
               </div>
+              <div>
+                By clicking Continue with or Sign Up, you agree to LangJo <br />
+                <span className="font-weight-bold">
+                  Terms of Service and Privacy Policy
+                </span>
+                &nbsp; &nbsp;
+              </div>
+              <span className="font-weight-bold">&nbsp; &nbsp; </span>
             </div>
           </div>
         </div>

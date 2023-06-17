82e813d1b5ca3b50a86945bc3f618a68965ba405
kavitappatil <patil.poem@gmail.com>
2023-06-17 22:23:39 +0100

    signup page removed commented lines

--- langjo-frontend/src/Component/SignUp.js
+++ langjo-frontend/src/Component/SignUp.js
@@ -66,44 +66,6 @@ function SignUp() {
                         </button>
                       </div>
                     </form>
-                    {/* <form className="mx-1 mx-md-4">
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" className="form-control" />
-                      <label className="form-label" for="form3Example1c">Full Name</label>
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" className="form-control" />
-                      <label className="form-label" for="form3Example3c">Email</label>
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" className="form-control" />
-                      <label className="form-label" for="form3Example4c">Password</label>
-                    </div>
-                  </div>
-
-                  <div className="form-check d-flex justify-content-center mb-5">
-                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                    <label className="form-check-label" for="form2Example3">
-                      I agree all statements in <a href="#!">Terms of service</a>
-                    </label>
-                  </div>
-
-                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" className="btn btn-primary btn-lg">Register</button>
-                  </div>
-
-                </form> */}
                   </div>
                   <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-2 order-lg-2">
                     <img

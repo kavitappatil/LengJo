8edaa57581667d12247ac9a1d86f6aa776b1be9f
kavitappatil <patil.poem@gmail.com>
2023-06-27 13:02:50 +0100

    Sign up page validation added

--- langjo-frontend/src/Component/SignUp.js
+++ langjo-frontend/src/Component/SignUp.js
@@ -1,94 +1,194 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./SignUp.css";
 import langJo from "../Images/langJo.png";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 function SignUp() {
+  	const [isValid, setIsValid] = useState(true);
+
+  	const [formData, setFormData] = useState({
+      full_name: "",
+      email: "",
+      password: "",
+    });
+  
+
+   const resetForm = () => {
+		setFormData({
+      full_name: "",
+      email: "",
+      password: "",
+    });
+	};
+
+  const handleInputChange = (event) => {
+    let { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  useEffect(() => {
+    //toast("There was an error loading this page.");
+  }, []);
+
+  const handleSubmit = (event) => {
+		event.preventDefault();
+
+		if (
+      formData.full_name.trim() === "" ||
+      formData.email.trim() === "" ||
+      formData.password.trim() === ""
+    ) {
+      setIsValid(false);
+      return;
+    }
+  };
+
   return (
-    <section className="background vh-100">
-      <div className="container h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-lg-12 col-xl-11">
-            <div className="card text-black card-color">
-              <div className="card-body p-md-5">
-                <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-1 order-lg-1">
-                    <p className="text-start h2 sign_up-heading fw-bold p-2">
-                      Sign up
-                    </p>
-                    <span className="form-group d-flex flex-row p-2">
-                      Already have an account?{" "}
-                      <span className="login">&nbsp; Log in</span>
-                    </span>
-                    &nbsp;
-                    <form>
-                      <div className="form-group ">
-                        <div className="d-flex flex-row p-2">
-                          <label for="exampleInputPassword1 d-flex  ">
-                            Full Name
-                          </label>
-                        </div>
-                        <input
-                          type="name"
-                          className="form-control w-75"
-                          id="exampleInputName"
-                          placeholder="Name"
-                        />
-                      </div>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <section className="background vh-100">
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black card-color">
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-1 order-lg-1">
+                      <p className="text-start h2 sign_up-heading fw-bold p-2">
+                        Sign up
+                      </p>
+                      <span className="form-group d-flex flex-row p-2">
+                        Already have an account?{" "}
+                        <span className="login">&nbsp; Log in</span>
+                      </span>
                       &nbsp;
-                      <div className="form-group">
-                        <div className="d-flex flex-row p-2">
-                          <label for="exampleInputEmail1">Email address</label>
+                      <form
+                        className={`w-75 requires-validation ${
+                          !isValid && "was-validated"
+                        }`}
+                        noValidate
+                        onSubmit={handleSubmit}
+                      >
+                        <div className="form-group ">
+                          <div className="d-flex flex-row p-2">
+                            <label for="exampleName d-flex  ">
+                              Full Name
+                            </label>
+                          </div>
+                          <input
+                            type="text"
+                            name="full_name"
+                            className="form-control w-75"
+                            id="full_name"
+                            placeholder="Name"
+                            onChange={handleInputChange}
+                            required
+                            value={formData.full_name}
+                          />
+                          <div id="validationName" className="invalid-feedback">
+                            Please enter your name.
+                          </div>
                         </div>
-                        <input
-                          type="email"
-                          className="form-control w-75"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter email"
-                        />
-                      </div>
-                      &nbsp;
-                      <div className="form-group">
-                        <div className="d-flex flex-row p-2">
-                          <label for="exampleInputPassword1">Password</label>
+                        &nbsp;
+                        <div className="form-group">
+                          <div className="d-flex flex-row p-2">
+                            <label for="exampleInputEmail1">
+                              Email address
+                            </label>
+                          </div>
+                          <input
+                            type="email"
+                            className="form-control w-75"
+                            name="email"
+                            id="email"
+                            placeholder="Enter email address "
+                            maxLength="500"
+                            onChange={handleInputChange}
+                            required
+                            value={formData.email}
+                          />
+                          <div
+                            id="validationEmail"
+                            className="invalid-feedback"
+                          >
+                            Please enter your email address.
+                          </div>
                         </div>
-                        <input
-                          type="password"
-                          className="form-control w-75"
-                          id="exampleInputPassword1"
-                          placeholder="Password"
-                        />
-                      </div>
-                      <br />
-                      <div className="row d-flex justify-content-start p-3">
-                        <button type="submit" className="btn sign_up-btn w-75">
-                          Sign Up
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-2 order-lg-2">
-                    <img
-                      src={langJo}
-                      className="img-fluid"
-                      alt="Sample image"
-                    />
+                        &nbsp;
+                        <div className="form-group">
+                          <div className="d-flex flex-row p-2">
+                            <label for="exampleInputPassword1">Password</label>
+                          </div>
+                          <input
+                            type="password"
+                            className="form-control w-75"
+                            id="exampleInputPassword1"
+                            placeholder="Enter Password"
+                            className="form-control w-75"
+                            name="email"
+                            id="email"
+                            maxLength="200"
+                            onChange={handleInputChange}
+                            required
+                            value={formData.password}
+                          />
+                          <div
+                            id="validationPassword"
+                            className="invalid-feedback"
+                          >
+                            Please enter your password.
+                          </div>
+                        </div>
+                        <br />
+                        <div className="row d-flex justify-content-start p-3">
+                          <button
+                            type="submit"
+                            className="btn sign_up-btn w-75"
+                          >
+                            Sign Up
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-2 order-lg-2">
+                      <img
+                        src={langJo}
+                        className="img-fluid"
+                        alt="Sample image"
+                      />
+                    </div>
                   </div>
                 </div>
+                <div>
+                  By clicking Continue with or Sign Up, you agree to LangJo{" "}
+                  <br />
+                  <span className="font-weight-bold">
+                    Terms of Service and Privacy Policy
+                  </span>
+                  &nbsp; &nbsp;
+                </div>
+                <span className="font-weight-bold">&nbsp; &nbsp; </span>
               </div>
-              <div>
-                By clicking Continue with or Sign Up, you agree to LangJo <br />
-                <span className="font-weight-bold">
-                  Terms of Service and Privacy Policy
-                </span>
-                &nbsp; &nbsp;
-              </div>
-              <span className="font-weight-bold">&nbsp; &nbsp; </span>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
+
 export default SignUp;

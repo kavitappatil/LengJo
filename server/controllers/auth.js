738cccdb36cda069363480ac7d625593fa29b18e
JanefrancessC <janefrancesschioma@gmail.com>
2023-06-28 08:39:32 +0100

    signup completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,16 +2,16 @@ import mongoose from 'mongoose';
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User({ ...req.body, password: hash })
-        
+
         await newUser.save();
         res.status(200).send("User created successfully");
 
     } catch (error) {
-        //handle error
+        next(error);
     }
 };

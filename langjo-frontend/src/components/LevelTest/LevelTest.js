49089142f7f9e8e316f67c4c6e52fc36974b0847
Pakize Bozkurt <pakiozihak@gmail.com>
2023-07-14 11:05:23 +0100

    updated link

--- langjo-frontend/src/components/LevelTest/LevelTest.js
+++ langjo-frontend/src/components/LevelTest/LevelTest.js
@@ -1,48 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LevelTest.css";
-import iconGif from "./english-level-icon.gif";
-import todoGif from "./todo-list.gif";
 
 function LevelTest() {
-  const handleLinkClick = () => {
-    window.open("https://www.britishcouncil.org/exam", "_blank");
-  };
+
   return (
-    <div className="container mt-auto level-test-container h-100 w-100">
-      <div className="row d-flex justify-content-center align-items-center h-100 w-100">
-        <div className="row justify-content-center w-100">
-          <div className=" col-lg-3">
-            <div className="card rounded-4 w-100 custom-card">
-              <div className="card-body d-flex flex-column align-items-center">
-                <img src={iconGif} alt="English Level Icon" className="icon" />
-                <h5 className="card-title level-test-heading">
-                  Understand English Level
-                </h5>
-                <Link
-                  to="/englishlevel"
-                  className="btn btn-outline-dark align-items-center bg-body-tertiary border rounded-4 level-test-button"
-                >
-                  Click Learn
-                </Link>
-              </div>
+    <div className=" py-5 my-3 text-primary-emphasis mx-5 level-test-container">
+      <div className="d-flex justify-content-center align-items-center h-100">
+        <div className="row align-items-md-stretch">
+          <div className="col-md-5">
+            <div className="h-100 p-5 bg-body-tertiary border rounded-5">
+              <h2 className="level-test-heading fs-5">
+                Understand English Level
+              </h2>
+              <Link
+                to="/englishlevel"
+                className="btn btn-outline-dark  bg-body-tertiary border rounded-4 my-3 level-test-link"
+              >
+                Click Learn
+              </Link>
             </div>
           </div>
-          <div className=" col-lg-3">
-            <div className="card rounded-4 w-100 custom-card">
-              <div className="card-body d-flex flex-column align-items-center">
-                <img src={todoGif} alt="Todo List Icon" className="icon" />
-                <h5 className="card-title level-test-heading">
-                  Free Online English Test
-                </h5>
+          <div className="col-md-5">
+            <div className="h-100 p-5 bg-body-tertiary border rounded-5">
+              <h2 className="level-test-heading fs-5">
+                Free Online English Test
+              </h2>
+              <a
+                href="https://www.britishcouncil.org/exam"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button
-                  className="btn btn-outline-dark align-items-center bg-body-tertiary border rounded-4 level-test-button"
+                  className="btn btn-outline-dark bg-body-tertiary border rounded-4 my-3 level-test-link"
                   type="button"
-                  onClick={handleLinkClick}
+                  
                 >
-                  Take a test
+                  Take an exam
                 </button>
-              </div>
+              </a>
             </div>
           </div>
         </div>

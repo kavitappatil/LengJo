adc4059cc8c9125f63767f6723db8e1bcad44afd
Pakize Bozkurt <pakiozihak@gmail.com>
2023-07-26 14:26:23 +0100

    fixed some  paths

--- langjo-frontend/src/pages/Pages.js
+++ langjo-frontend/src/pages/Pages.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import BeginnerLevel from "./components/BeginnerLevel/BeginnerLevel";
-import EnglishLevel from "./components/EnglishLevel/EnglishLevel";
-import BlockCards from "./components/BlockCards/BlocksCards";
-import Home from "./Home.js";
+import BeginnerLevel from "../components/BeginnerLevel/BeginnerLevel";
+import EnglishLevel from "../components/EnglishLevel/EnglishLevel";
+import BlockCards from "../components/BlockCards/BlocksCards";
+import Home from "../Home.js";
 
 function Pages() {
   return (

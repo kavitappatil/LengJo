67c98a5185955719416480f483e6c7b7d33b926e
kavitappatil <patil.poem@gmail.com>
2023-08-15 10:30:28 +0100

    created skeleton of this section

--- langjo-frontend/src/components/VideoDetails/VideoDetails.js
+++ langjo-frontend/src/components/VideoDetails/VideoDetails.js
@@ -76,67 +76,6 @@ function VideoDetails() {
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body">
-              <strong>Intro </strong>welcome to lovely english stories thank you
-              for stopping by this story is perfect for beginner learners let's
-              start english story a <br />{" "}
-              <strong>The Birthday Party 2 </strong>the birthday party it is a
-              special day for emma and her twin sister lauren it is their
-              birthday lauren is the oldest twin she was born six minutes before
-              emma today they are turning 21. lauren is happy about it but emma
-              is not emma is enjoying being a university student and does not
-              want to feel old lauren can't wait to finish university and wants
-              to be an adult you are only 21 wants so lauren and emma have
-              planned a special day they will have pancakes for breakfast with
-              their family they always have pancakes on their birthday it is a
-              family tradition emma and lauren's dad makes amazing pancakes he
-              always serves them with fresh fruit after breakfast they will
-              visit their grandparents both emma and lauren are very close to
-              their grandparents then they will go to the supermarket to pick up
-              some very important birthday supplies at their party they will
-              have a typical british buffet with sandwiches sausage rolls cakes
-              crisps and biscuits they have invited a lot of people to their
-              party so they will have vegan and vegetarian options too they also
-              have party bags for the children with little toys inside emma's
-              friend is a baker so she is going to make the girls a very special
-              21st birthday cake it will be multi-colored and have sweets inside
-              ever since they were little girls they have wanted a cake like
-              this so why not have one on their 21st birthday emma's friend will
-              also make some cupcakes everyone loves cupcakes they have not
-              booked a dj they will make their own party playlist on youtube or
-              spotify that way they can choose their favorite music they have
-              invited over 50 people to their party the guests are their friends
-              family members and work colleagues from their part time jobs they
-              hope that everyone will get on and have a good time they will have
-              the party in their parents garden luckily they live in the
-              countryside and so they have a huge garden they have a big gazebo
-              at the bottom of the garden so if the weather is bad they will
-              have somewhere to go emma has organized some party games and
-              lauren is in charge of the drinks she has ordered a mix of
-              alcoholic and non alcoholic drinks lauren has also ordered some
-              lights balloons and birthday decorations for the garden they have
-              invited their parents neighbors so they don't have to worry about
-              making too much noise they are really excited now they just have
-              to organize everything before the guests arrive they have three
-              hours before the party starts they start to make the food for the
-              buffet and their mum and dad organize the garden three hours later
-              lauren and emma are ready just in time for their first guests
-              arriving they have decorated the garden made the food and set up
-              the dance area with music and lights they have put on new outfits
-              and have their first glass of champagne with their friends luckily
-              emma and lauren were born in july so the weather is warm and sunny
-              for now the guests enjoy the cakes and food and their family dance
-              to the music the party is a huge success lauren and emma's parents
-              have organized a surprise for them
-              <br /> <strong>Fireworks</strong> fireworks as soon as it is dark
-              emma and lauren's parents ask everyone to walk to the end of the
-              garden and tell them to look up towards the sky lauren and emma
-              are surprised when the sky lights up with beautiful fireworks the
-              display is beautiful just as it ends it starts to rain a little
-              typical english weather luckily they have the gazebo some of the
-              guests go inside the house and the others party under the gazebo
-              until it's time to go home we hope you enjoyed this lovely english
-              story thank you for stopping by don't forget to like subscribe and
-              share you
             </div>
           </div>
         </div>
@@ -195,13 +134,8 @@ function VideoDetails() {
               >
                 <div className="carousel-inner">
                   <div className="carousel-item active carousel-item-start">
-                    <div>
-                      How old are Emma and Lauren turning in the story? a) 18{" "}
-                      <br />
-                      b) 21 <br />
-                      c) 25 <br />
-                      d) 30
-                    </div>
+                  
+                   
                   </div>
                   <div className="carousel-item carousel-item-next carousel-item-start">
                     <svg

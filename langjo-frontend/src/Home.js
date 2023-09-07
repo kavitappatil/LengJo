f97d04ec81103c02d0df8487e31a2d159e947139
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-07 21:12:08 +0100

    22-youtube-video ticket

--- langjo-frontend/src/Home.js
+++ langjo-frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import LevelTest from "./components/LevelTest/LevelTest";
 import BeginnerLevel from "./components/BeginnerLevel/BeginnerLevel";
-import BeginnerLevel from "./components/BeginnerLevel/BeginnerLevel";
 import BlocksCards from "./components/BlockCards/BlocksCards";
 import Hero from "./components/Hero/Hero";
 

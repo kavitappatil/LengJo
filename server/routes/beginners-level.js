79e4349dbff784667b57ad096267012b4a2c5ef6
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-13 10:08:01 +0100

    videopage

--- server/routes/beginners-level.js
+++ server/routes/beginners-level.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { displayVideo, createVideo } from "../controllers/video.js";
+import { getVideo, getVideoByTitle, displayVideo, createVideo } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 const app = express();
 
 app.use(
@@ -17,5 +18,8 @@ app.use(express.json());
 const videoRouter = express.Router();
 videoRouter.get("/beginners-level", displayVideo);
 videoRouter.post("/beginners-level", createVideo);
+videoRouter.get("/beginners-level/:videoId", getVideo);
+videoRouter.put("/beginners-level/:videoId", verifyToken);
+videoRouter.get("/title/:title", getVideoByTitle);
 
 export { videoRouter };

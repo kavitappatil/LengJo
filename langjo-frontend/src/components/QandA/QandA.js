f7037ad67166cfbd06b64ce8b9bc6c27a50c0097
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-15 21:33:04 +0100

    added style

--- langjo-frontend/src/components/QandA/QandA.js
+++ langjo-frontend/src/components/QandA/QandA.js
@@ -71,11 +71,11 @@ const QandA = () => {
             className="border border-5 border-end g-5 pe-5 ms-5 "
             key={index}
           >
-            <div className="fs-6 border-2 d-flex justify-content-start">
+            <div className=" d-flex justify-content-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
+                width="60"
+                height="60"
                 fill="navy"
                 className="bi bi-person-square roundend-end"
                 viewBox="0 0 16 16"
@@ -84,7 +84,7 @@ const QandA = () => {
                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"></path>
               </svg>
               <Card.Header as="h-6">
-                <small className="px-3">Submitted by {comment.username}</small>
+                <small className="px-4">Submitted by {comment.username}</small>
                 <hr />
               </Card.Header>
             </div>

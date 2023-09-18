85133d27bb3d5c307032fde52d2daaf67739a173
JanefrancessC <janefrancesschioma@gmail.com>
2023-09-18 09:46:43 +0100

    added a displayVideo function, to display only one video

--- server/routes/beginners-level.js
+++ server/routes/beginners-level.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { displayVideo, createVideo } from '../controllers/video.js';
+import { displayVideo, createVideo, displayVideos } from '../controllers/video.js';
 
 const videoRouter = express.Router();
-videoRouter.get('/beginners-level', displayVideo); 
+videoRouter.get('/beginners-level/:id', displayVideo); 
+videoRouter.get('/beginners-level', displayVideos);
 videoRouter.post('/beginners-level', createVideo); 
 
 export { videoRouter }

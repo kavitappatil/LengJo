5af19999b8c40b318aae6372d6bf128b2afbe7bd
kavitappatil <patil.poem@gmail.com>
2023-09-21 12:00:11 +0100

    model video file updated

--- server/models/Video.js
+++ server/models/Video.js
@@ -1,35 +1,106 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const VideoSchema = new mongoose.Schema({ 
-    title: { 
-        type: String,
-        required: [true, "title is required"],
-        unique: true,
+const VideoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+      unique: true,
     },
     desc: {
-        type: String,
-        required: [true, "Description is required"],
-        unique: true,
+      type: String,
+      required: [true, "Description is required"],
+      unique: true,
     },
     videoUrl: {
-        type: String,
-        required: [true, "Video link is required"],
-        unique: true,
+      type: String,
+      required: [true, "Video link is required"],
+      unique: true,
     },
     views: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     likes: {
-        type: [String],
-        default: [],
+      type: [String],
+      default: [],
     },
     dislikes: {
-        type: [String],
-        default: [],
-    }
-}, 
-{ timestamps: true });
-
+      type: [String],
+      default: [],
+    },
+    introduction: {
+      type: String,
+      required: [true, "introduction link is required"],
+      unique: true,
+    },
+    noun: {
+      type: String,
+      required: [true, "noun link is required"],
+      unique: true,
+    },
+    verb: {
+      type: String,
+      required: [true, "verb link is required"],
+      unique: true,
+    },
+    adjective: {
+      type: String,
+      required: [true, "adjective link is required"],
+      unique: true,
+    },
+    transcript: {
+      type: String,
+      required: [true, "transcript link is required"],
+      unique: true,
+    },
+    que1: {
+      type: String,
+      required: [true, "que1 link is required"],
+      unique: true,
+    },
+    ans1: {
+      type: String,
+      required: [true, "ans1 link is required"],
+      unique: true,
+    },
+    option1: {
+      type: String,
+      required: [true, "option1 link is required"],
+      unique: true,
+    },
+    que2: {
+      type: String,
+      required: [true, "que2 link is required"],
+      unique: true,
+    },
+    ans2: {
+      type: String,
+      required: [true, "ans2 link is required"],
+      unique: true,
+    },
+    option2: {
+      type: String,
+      required: [true, "option2 link is required"],
+      unique: true,
+    },
+    que3: {
+      type: String,
+      required: [true, "que3 link is required"],
+      unique: true,
+    },
+    ans3: {
+      type: String,
+      required: [true, "ans3 link is required"],
+      unique: true,
+    },
+    option3: {
+      type: String,
+      required: [true, "option3 link is required"],
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Video", VideoSchema);
